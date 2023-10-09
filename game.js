f2eaae17fcd59b7334a2fcb5a106b41cf36f395e
Ilia <iliiliusiki@gmail.com>
2023-10-09 10:21:41 +0300

    muve game to browser

--- game.js
+++ game.js
@@ -1,7 +1,14 @@
-import { prompt, sample } from './utils.js'
+import { sample } from './utils.js'
 
 const words = ['car', 'cat', 'pen', 'pencil', 'open', 'close','dog', 'room', 'samsung', 'iphone']
-const hiddenWord = sample(words)
-// console.log(`hiddenWord: ${hiddenWord}`)
-console.log('I thought of a word, guess it.')
-let input = await prompt('What is your word?')
+const hiddenWord = sample(words.slice(0, 9))
+
+const init = () => {
+  // console.log('I thought of a word, guess it, please.')
+
+  const word = prompt('Enter your word.')
+  const guessed = word == hiddenWord
+  console.log(guessed ? 'you win!' : 'you loose.')
+}
+
+document.addEventListener('DOMContentLoaded', init)
--- utils.js
+++ utils.js
@@ -1,11 +1,3 @@
-import * as readline from 'node:readline/promises'
-import { stdin as input, stdout as output } from 'node:process'
-
 export const sample = (items) => {
   return items[Math.floor(Math.random() * items.length)]
-}
-
-export const prompt = async () => {
-  const rl = readline.createInterface({ input, output });
-  return await rl.question('What do you think of Node.js? ');
 }
