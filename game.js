3061b4e0104e8cdfb3087a262418d5436b2cbbe8
Ilia <iliiliusiki@gmail.com>
2023-10-09 11:01:09 +0300

    Add ESLint

--- game.js
+++ game.js
@@ -1,13 +1,12 @@
 import { sample } from './utils.js'
-
-const words = ['car', 'cat', 'pen', 'pencil', 'open', 'close','dog', 'room', 'samsung', 'iphone']
-const hiddenWord = sample(words.slice(0, 9))
+const words = ['car', 'cat', 'pen', 'pencil', 'open', 'close', 'dog', 'room', 'samsung', 'iphone']
+const hiddenWord = sample(words.slice(0, 9) )
 
 const init = () => {
   // console.log('I thought of a word, guess it, please.')
 
   const word = prompt('Enter your word.')
-  const guessed = word == hiddenWord
+  const guessed = word === hiddenWord
   console.log(guessed ? 'you win!' : 'you loose.')
 }
 
