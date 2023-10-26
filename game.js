d7e20d01c33220e7f921b5fb62aac5b1a7cc7df4
Ilia <iliiliusiki@gmail.com>
2023-10-26 12:46:46 +0500

    new game added - Stone, Scissors, Paper

--- game.js
+++ game.js
@@ -1,35 +1,9 @@
-import { clear, print, sample } from './utils.js'
-const words = ['car', 'cat', 'pen', 'pencil', 'open', 'close', 'dog', 'room', 'samsung', 'iphone']
-let hiddenWord
+import { GuessWordGame } from './lib/guess_word_game.js'
+import { StoneScissorsPaperGame } from './lib/stone_scissors_paper_game.js'
 
-const startNewGame = () => {
-  clear('.game-block--guess-word #container')
-  hiddenWord = sample(words.slice(0, 3))
+const start = () => {
+  document.addEventListener('DOMContentLoaded', new GuessWordGame().init)
+  document.addEventListener('DOMContentLoaded', new StoneScissorsPaperGame().init)
 }
 
-const startButton = () => {
-  return document.querySelector('.game-block--guess-word button#start-new-game')
-}
-
-const wordInput = () => {
-  return document.querySelector('.game-block--guess-word input#word')
-}
-
-const check = () => {
-  const word = wordInput().value
-  const guessed = word === hiddenWord
-  print(
-    '.game-block--guess-word #container',
-    guessed ?
-      `Congratulations! You win! The word '${word}' is right` :
-      `Sorry, the word '${word}' is wrong`
-  )
-}
-
-const init = () => {
-  startNewGame()
-  startButton().addEventListener('click', startNewGame)
-  wordInput().addEventListener('change', check)
-}
-
-document.addEventListener('DOMContentLoaded', init)
+start()
